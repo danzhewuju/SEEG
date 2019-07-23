16ab4b54257e3101f84aaa6a9839ec3aa5875b1c
Alex <1668926294@qq.com>
2019-07-23 20:09:33 +0800

    add variational auto-encode function

--- VAE/vae.py
+++ VAE/vae.py
@@ -1,51 +1,135 @@
 from __future__ import print_function
 
 import argparse
+import os
 
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
 import torch.utils.data
 from torch import nn, optim
 from torch.nn import functional as F
-from torchvision import datasets, transforms
+from torch.utils.data import Dataset
 from torchvision.utils import save_image
+
+from util.util_file import matrix_normalization
 
 parser = argparse.ArgumentParser(description='VAE MNIST Example')
 parser.add_argument('--batch-size', type=int, default=128, metavar='N',
                     help='input batch size for training (default: 128)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
+parser.add_argument('--epochs', type=int, default=5, metavar='N',
                     help='number of epochs to train (default: 10)')
 parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='enables CUDA training')
+parser.add_argument('-train_p', '--train_path', default='../data/seeg/zero_data/train')
+parser.add_argument('-test_p', '--test_path', default='../data/seeg/zero_data/test')
+parser.add_argument('-val_p', '--val_path', default='../data/seeg/zero_data/val')
 parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
 parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                     help='how many batches to wait before logging training status')
 args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
-
+TEST_PATH = args.test_path
+TRAIN_PATH = args.train_path
+VAL_PATH = args.val_path
 torch.manual_seed(args.seed)
 
 device = torch.device("cuda" if args.cuda else "cpu")
 
 kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
-train_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('../data', train=True, download=True,
-                   transform=transforms.ToTensor()),
-    batch_size=args.batch_size, shuffle=True, **kwargs)
-test_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('../data', train=False, transform=transforms.ToTensor()),
-    batch_size=args.batch_size, shuffle=True, **kwargs)
+
+resize = (130, 200)
+
+
+# 数据的输入输出
+
+class Data_info():
+    def __init__(self, path_train, path_test):
+        index_name_train = os.listdir(path_train)
+        index_name_test = os.listdir(path_test)
+        data_train = []
+        data_test = []
+        preseizure = []
+        sleep_normal = []
+        for index, name in enumerate(index_name_train):
+            path = os.path.join(path_train, name)
+            dir_names = os.listdir(path)
+            for n in dir_names:
+                full_path = os.path.join(path, n)
+                if name == "pre_zeizure":
+                    preseizure.append(full_path)
+                else:
+                    sleep_normal.append(full_path)
+                data_train.append((full_path, index))
+
+        for index, name in enumerate(index_name_test):
+            path = os.path.join(path_test, name)
+            dir_names = os.listdir(path)
+            for n in dir_names:
+                full_path = os.path.join(path, n)
+                if name == "pre_zeizure":
+                    preseizure.append(full_path)
+                else:
+                    sleep_normal.append(full_path)
+                data_test.append((full_path, index))
+
+        # t = time.time()
+        # random.seed(t)
+        # random.shuffle(data_train)
+        # t = time.time()
+        # random.seed(t)
+        # random.shuffle(data_test)
+
+        self.data_train = data_train
+        self.data_test = data_test
+        self.train_length = len(data_train)
+        self.test_length = len(data_test)
+        self.preseizure = preseizure
+        self.sleep_normal = sleep_normal
+
+
+class MyDataset(Dataset):  # 重写dateset的相关类
+    def __init__(self, imgs, transform=None, target_transform=None):
+        self.imgs = imgs
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __getitem__(self, index):
+        fn, label = self.imgs[index]
+        data = np.load(fn)
+        result = matrix_normalization(data, (130, 200))
+        result = result.astype('float32')
+        result = result[np.newaxis, :]
+        return result, label
+
+    def __len__(self):
+        return len(self.imgs)
+
+
+datas = Data_info(path_train=TRAIN_PATH, path_test=TEST_PATH)
+positive_loader = MyDataset(datas.preseizure)  # 作为训练集
+negative_loader = MyDataset(datas.sleep_normal)  # 作为测试集
+
+
+# train_loader = torch.utils.data.DataLoader(
+#     datasets.MNIST('../data', train=True, download=True,
+#                    transform=transforms.ToTensor()),
+#     batch_size=args.batch_size, shuffle=True, **kwargs)
+# test_loader = torch.utils.data.DataLoader(
+#     datasets.MNIST('../data', train=False, transform=transforms.ToTensor()),
+#     batch_size=args.batch_size, shuffle=True, **kwargs)
 
 
 class VAE(nn.Module):
     def __init__(self):
         super(VAE, self).__init__()
 
-        self.fc1 = nn.Linear(784, 400)
+        self.fc1 = nn.Linear(resize[0] * resize[1], 400)
         self.fc21 = nn.Linear(400, 20)
         self.fc22 = nn.Linear(400, 20)
         self.fc3 = nn.Linear(20, 400)
-        self.fc4 = nn.Linear(400, 784)
+        self.fc4 = nn.Linear(400, resize[0] * resize[1])
 
     def encode(self, x):
         h1 = F.relu(self.fc1(x))
@@ -61,7 +145,7 @@ class VAE(nn.Module):
         return torch.sigmoid(self.fc4(h3))
 
     def forward(self, x):
-        mu, logvar = self.encode(x.view(-1, 784))
+        mu, logvar = self.encode(x.view(-1, resize[0] * resize[1]))
         z = self.reparameterize(mu, logvar)
         return self.decode(z), mu, logvar
 
@@ -72,7 +156,7 @@ optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
 # Reconstruction + KL divergence losses summed over all elements and batch
 def loss_function(recon_x, x, mu, logvar):
-    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1, resize[0] * resize[1]), reduction='sum')
 
     # see Appendix B from VAE paper:
     # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
@@ -83,10 +167,11 @@ def loss_function(recon_x, x, mu, logvar):
     return BCE + KLD
 
 
-def train(epoch):
+def train_negative(epoch):
     model.train()
     train_loss = 0
-    for batch_idx, (data, _) in enumerate(train_loader):
+    for batch_idx, (data, _) in enumerate(negative_loader):
+        data = torch.from_numpy(data)
         data = data.to(device)
         optimizer.zero_grad()
         recon_batch, mu, logvar = model(data)
@@ -96,39 +181,118 @@ def train(epoch):
         optimizer.step()
         if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                       100. * batch_idx / len(train_loader),
+                epoch, batch_idx * len(data), len(datas.sleep_normal),
+                       100. * batch_idx / len(negative_loader),
                        loss.item() / len(data)))
 
     print('====> Epoch: {} Average loss: {:.4f}'.format(
-        epoch, train_loss / len(train_loader.dataset)))
+        epoch, train_loss / datas.train_length))
+    name = str("./models/model-vae-negative.ckpt")
+    torch.save(model.state_dict(), name)
+    print("model has been saved!")
 
 
-def test(epoch):
-    model.eval()
-    test_loss = 0
-    with torch.no_grad():
-        for i, (data, _) in enumerate(test_loader):
-            data = data.to(device)
-            recon_batch, mu, logvar = model(data)
-            test_loss += loss_function(recon_batch, data, mu, logvar).item()
-            if i == 0:
-                n = min(data.size(0), 8)
-                comparison = torch.cat([data[:n],
-                                        recon_batch.view(args.batch_size, 1, 28, 28)[:n]])
-                save_image(comparison.cpu(),
-                           'results/reconstruction_' + str(epoch) + '.png', nrow=n)
+def train_positive(epoch):
+    model.train()
+    train_loss = 0
+    for batch_idx, (data, _) in enumerate(positive_loader):
+        data = torch.from_numpy(data)
+        data = data.to(device)
+        optimizer.zero_grad()
+        recon_batch, mu, logvar = model(data)
+        loss = loss_function(recon_batch, data, mu, logvar)
+        loss.backward()
+        train_loss += loss.item()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(datas.positive_loader),
+                       100. * batch_idx / len(negative_loader),
+                       loss.item() / len(data)))
 
-    test_loss /= len(test_loader.dataset)
-    print('====> Test set loss: {:.4f}'.format(test_loss))
+    print('====> Epoch: {} Average loss: {:.4f}'.format(
+        epoch, train_loss / datas.train_length))
+    name = str("./models/model-vae-positive.ckpt")
+    torch.save(model.state_dict(), name)
+    print("model has been saved!")
+
+
+def trans_data(vae_model, data, shape=(130, 200)):
+    data_tmp = torch.from_numpy(data)
+    data_input = data_tmp.cuda(0)
+    recon_batch, _, _ = vae_model(data_input)
+    recon_batch = recon_batch.cpu()
+    result = recon_batch.detach().numpy()
+    result = result.reshape(shape)
+    result = result[np.newaxis, :]
+    return result
+
+
+def show_eeg(data):
+    plt.imshow(data)
+    plt.show()
+
+
+# def vae_test(epoch):
+#     model.eval()
+#     test_loss = 0
+#     with torch.no_grad():
+#         for i, (data, _) in enumerate(test_loader):
+#             data = torch.from_numpy(data)
+#             data = data.to(device)
+#             recon_batch, mu, logvar = model(data)
+#             test_loss += loss_function(recon_batch, data, mu, logvar).item()
+#             if i == 0:
+#                 n = min(data.size(0), 8)
+#                 comparison = torch.cat([data[:n],
+#                                         recon_batch.view(1, resize[0], resize[1])[:n]])
+#                 save_image(comparison.cpu(),
+#                            'results/reconstruction_' + str(epoch) + '.png', nrow=n)
+#
+#     test_loss /= datas.test_length
+#     print('====> Test set loss: {:.4f}'.format(test_loss))
 
 
 if __name__ == "__main__":
     for epoch in range(1, args.epochs + 1):
-        train(epoch)
-        test(epoch)
-        with torch.no_grad():
-            sample = torch.randn(64, 20).to(device)
-            sample = model.decode(sample).cpu()
-            save_image(sample.view(64, 1, 28, 28),
-                       'results/sample_' + str(epoch) + '.png')
+        train_positive(epoch)
+        # vae_test(epoch)
+
+# model = VAE().to(device)
+# path = "./models/model-vae.ckpt"
+# model.load_state_dict(torch.load(path))
+# vae_test(1)
+
+# path = "../data/seeg/zero_data/train/sleep_normal/12110f04-a840-11e9-893a-e197cbdc17cd-2.npy"
+# data = np.load(path)
+# model_path = "./models/model-vae.ckpt"
+# model.load_state_dict(torch.load(model_path))
+# show_eeg(data)
+# print(data.shape)
+# result = matrix_normalization(data, (130, 200))
+# result = result.astype('float32')
+# result = result[np.newaxis, :]
+# for i in range(16):
+#     result_image = trans_data(model, result)
+#     result_image = result_image.reshape(130, 200)
+#     plt.subplot(4, 4, i + 1)
+#     plt.imshow(result_image)
+#     # show_eeg(result_image)
+# plt.show()
+# if __name__ == '__main__':
+#     path = "/home/cbd109-3/Users/data/yh/Program/Python/SEEG/VAE/models/model-vae.ckpt"  # 模型所在的位置
+#     vae_model = VAE()
+#     vae_model.load_state_dict(torch.load(path))
+
+
+# 测试的样本
+# path = "../data/seeg/zero_data/train/sleep_normal/12110f04-a840-11e9-893a-e197cbdc17cd-2.npy"
+# model_path = "./models/model-vae.ckpt"
+# model.load_state_dict(torch.load(model_path))
+# with torch.no_grad():
+#     sample = torch.randn(1, 20).to(device)
+#     print(sample)
+#     sample = model.decode(sample).cpu()
+#     sample = sample.reshape(130, 200)
+#     plt.imshow(sample)
+#     plt.show()
