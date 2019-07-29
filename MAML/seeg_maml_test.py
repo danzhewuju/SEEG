8375e95e0d906f00e5999b16e516521ed177c470
Alex <1668926294@qq.com>
2019-07-29 22:52:23 +0800

    模型参数优化

--- MAML/seeg_maml_test.py
+++ MAML/seeg_maml_test.py
@@ -33,7 +33,7 @@ def main():
 
     config = [
         ('conv2d', [32, 1, 3, 3, 1, 0]),
-        ('relu', [True]),
+        ('relu', [True]),1
         ('bn', [32]),
         ('max_pool2d', [2, 2, 0]),
         ('conv2d', [32, 32, 3, 3, 1, 0]),
@@ -99,9 +99,9 @@ if __name__ == '__main__':
     argparser.add_argument('--n_way', type=int, help='n way', default=2)
     argparser.add_argument('--k_spt', type=int, help='k shot for support set', default=10)
     argparser.add_argument('--k_qry', type=int, help='k shot for query set', default=10)
-    argparser.add_argument('--imgsz', type=int, help='imgsz', default=84)
-    argparser.add_argument('--imgc', type=int, help='imgc', default=3)
-    argparser.add_argument('--task_num', type=int, help='meta batch size, namely task num', default=6)
+    argparser.add_argument('--imgsz', type=int, help='imgsz', default=100)
+    argparser.add_argument('--imgc', type=int, help='imgc', default=5)
+    argparser.add_argument('--task_num', type=int, help='meta batch size, namely task num', default=5)
     argparser.add_argument('--meta_lr', type=float, help='meta-level outer learning rate', default=1e-3)
     argparser.add_argument('--update_lr', type=float, help='task-level inner update learning rate', default=0.01)
     argparser.add_argument('--update_step', type=int, help='task-level inner update steps', default=5)
--- MAML/seeg_maml_train.py
+++ MAML/seeg_maml_train.py
@@ -20,9 +20,9 @@ def mean_confidence_interval(accs, confidence=0.95):
 
 
 def main():
-    # torch.manual_seed(222)  # 为cpu设置种子，为了使结果是确定的
-    # torch.cuda.manual_seed_all(222)  # 为GPU设置种子，为了使结果是确定的
-    # np.random.seed(222)
+    torch.manual_seed(222)  # 为cpu设置种子，为了使结果是确定的
+    torch.cuda.manual_seed_all(222)  # 为GPU设置种子，为了使结果是确定的
+    np.random.seed(222)
 
     print(args)
 
@@ -140,9 +140,9 @@ if __name__ == '__main__':
     argparser.add_argument('--n_way', type=int, help='n way', default=2)
     argparser.add_argument('--k_spt', type=int, help='k shot for support set', default=10)
     argparser.add_argument('--k_qry', type=int, help='k shot for query set', default=10)
-    argparser.add_argument('--imgsz', type=int, help='imgsz', default=84)
-    argparser.add_argument('--imgc', type=int, help='imgc', default=3)
-    argparser.add_argument('--task_num', type=int, help='meta batch size, namely task num', default=6)
+    argparser.add_argument('--imgsz', type=int, help='imgsz', default=100)
+    argparser.add_argument('--imgc', type=int, help='imgc', default=5)
+    argparser.add_argument('--task_num', type=int, help='meta batch size, namely task num', default=5)
     argparser.add_argument('--meta_lr', type=float, help='meta-level outer learning rate', default=1e-3)
     argparser.add_argument('--update_lr', type=float, help='task-level inner update learning rate', default=0.01)
     argparser.add_argument('--update_step', type=int, help='task-level inner update steps', default=5)
