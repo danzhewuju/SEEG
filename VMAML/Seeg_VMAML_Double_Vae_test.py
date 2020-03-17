a1711ab9aedfa66baa18b7b7e62341e5514a3d8b
Alex <danyuhao@qq.com>
2020-03-17 12:50:21 +0800

    testing patient WSH

--- VMAML/Seeg_VMAML_Double_Vae_test.py
+++ VMAML/Seeg_VMAML_Double_Vae_test.py
@@ -37,7 +37,7 @@ argparser.add_argument('--task_num', type=int, help='meta batch size, namely tas
 argparser.add_argument('--meta_lr', type=float, help='meta-level outer learning rate', default=1e-3)
 argparser.add_argument('--update_lr', type=float, help='task-level inner update learning rate', default=0.01)
 argparser.add_argument('--update_step', type=int, help='task-level inner update steps', default=5)
-argparser.add_argument('--update_step_test', type=int, help='update steps for finetunning', default=10)
+argparser.add_argument('--update_step_test', type=int, help='update steps for finetunning', default=5)
 argparser.add_argument('--dataset_dir', type=str, help="training data set",
                        default="../data/seeg/zero_data/{}/".format(patient_test))
 
